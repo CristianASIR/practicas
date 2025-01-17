646648ec2f926b98ede3794b4e14d367f8e9331b
cristianasir2 <manzcris@gmail.com>
2025-01-17 10:17:42 +0100

    creando controlador con fichero .spec y sin fichero .spec

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,12 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/primera')
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('/productos')
+  getProductos(): string {
+    return this.appService.getProductos();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Primeras pruebas';
+  }
+  getProductos(): string {
+    return 'Listado de productos';
   }
 }
