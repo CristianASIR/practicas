caeb0987848a695082686a4b7835769cc7d7958b
cristianasir2 <manzcris@gmail.com>
2025-01-20 23:18:29 +0100

    .

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,16 +3,16 @@ import { Controller, Get } from '@nestjs/common';
 @Controller('products')
 export class ProductsController {
     // El orden en el que se encuentran estos "Decoradores" influye en la impresión de kas peticiones
-    @Get('primer')
-    findprimer() {
-        return "Primer decorador";
-    }
-    @Get('segundo')
-    findsegundo() {
-        return "Segundo decorador";
-    }
     @Get()
     getHelloInProducts(): string {
         return "Estamos en productos!!!";
+    }
+    @Get('primer')
+    findprimer():string {
+        return "Primer decorador";
+    }
+    @Get('primer/segundo')
+    findsegundo():string {
+        return "Segundo decorador";
     }
 }
