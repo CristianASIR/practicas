5f89f9e7e6a4f18a9e15399a07d1cbc856464148
cristianasir2 <manzcris@gmail.com>
2025-01-20 22:04:33 +0100

    modificacion de puertos

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get('/primera')
   getHello(): string {
@@ -13,4 +13,10 @@ export class AppController {
   getProductos(): string {
     return this.appService.getProductos();
   }
+  @Post()
+  createProduct(@Body() body){
+    return `Creo un producto ${body.articulo} con precio ${body.precio}`;
+    }
+    
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
-import { ProductsController } from './products/products.controller';
 
 @Module({
   imports: [],
