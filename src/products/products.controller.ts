0b40700b4c73bd973582d06a12cd3ea154dc2eec
cristianasir2 <manzcris@gmail.com>
2025-01-24 18:59:58 +0100

    .

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
-interface datos {id:string, size:string};
+interface datos { id: string, size: string };
 @Controller('products')
 export class ProductsController {
     // El orden en el que se encuentran estos "Decoradores" influye en la impresión de kas peticiones
@@ -21,15 +21,28 @@ export class ProductsController {
     findHot() {
         return "Productos en caliente"
     }
-
+    /*
+    //Anidar las funciones para que aparezcan dependiendo de su existencia
+    @Get(':id?/:size?')
+        findWithOptional(@Param() params: any) {
+        const { id,size } = params;
+        if (size && id) {
+        return `Item ${id} de tamaño ${size}`;
+        } else if (id) {
+        return `Todos los items de tamaño ${id}`;
+        } else {
+        return 'Todos los items';
+        }
+    }
+    */
     //Los parámetros generales se implementan en la última parte de los parámetros
     //Recoge el contenido del parámetro sea cual sea y lo ingresa en la ruta principal como una cadena
     @Get(':id')
-    findID(@Param()  parametros :any) :string{
+    findID(@Param() parametros: any): string {
         return `Productos obtenidos mediante parámetro ${parametros.id}`
     }
     @Get(':id/:size')
-    findIDSize(@Param() parametros:datos) :string{
+    findIDSize(@Param() parametros: datos): string {
         return `Productos obtenidos mediante parámetro ${parametros.size}`
     }
     @Post()
