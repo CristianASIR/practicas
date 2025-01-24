45f9e15a17e35a7651a7ce2fe2416874d908e808
cristianasir2 <manzcris@gmail.com>
2025-01-24 18:26:34 +0100

    Creando funciones generales

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -8,12 +8,26 @@ export class ProductsController {
         return "Totalidad de productos";
     }
     @Get('primer')
-    findprimer():string {
+    findprimer(): string {
         return "Primer decorador";
     }
     @Get('primer/segundo')
-    findsegundo():string {
+    findsegundo(): string {
         return "Segundo decorador";
+    }
+    //
+    @Get('hot')
+    findHot() {
+        return "Productos en caliente"
+    }
+    //Recoge el contenido del parámetro sea cual sea y lo ingresa en la ruta principal como una cadena
+    @Get(':id')
+    findID(@Param()  parametros :any) :string{
+        return `Productos obtenidos mediante parámetro ${parametros.id}`
+    }
+    @Get(':id/:size')
+    findIDSize(@Param()  parametros :any) :string{
+        return `Productos obtenidos mediante parámetro ${parametros.size}`
     }
     @Post()
     insertarProduct(): string {
