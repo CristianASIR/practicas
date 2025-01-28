a68030168b97be5289db09d702d54c5db85c917b
cristianasir2 <manzcris@gmail.com>
2025-01-28 20:58:02 +0100

    Configuración para acceder a la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 import { ClientesModule } from './clientes/clientes.module';
 import { GeneracionesModule } from './generaciones/generaciones.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -10,6 +11,16 @@ import { GeneracionesModule } from './generaciones/generaciones.module';
     ProductsModule,
     ClientesModule,
     GeneracionesModule,
+    TypeOrmModule.forRoot({
+      type:'mysql',
+      host:'localhost',
+      port:3306,
+      username:'root',
+      password:'usuario',
+      database:'apitarea',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize:true
+    })
   ],
   controllers: [],
   providers: [],
